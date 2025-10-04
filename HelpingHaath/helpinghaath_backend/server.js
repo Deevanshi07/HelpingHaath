f3dc8a2db8ef0ff2d4466989b11da0f3e3ac959d
Deevanshi <deevanshi991@gmail.com>
2025-10-04 21:47:29 +0530

    fix: remove duplicate multer upload declaration

--- HelpingHaath/helpinghaath_backend/server.js
+++ HelpingHaath/helpinghaath_backend/server.js
@@ -457,7 +457,12 @@ app.use((req, _res, next) => {
   next();
 });
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
